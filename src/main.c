8d2a4a6f19349de711b0d01b20cd1e060878e19f
Bohdan <bohdan.kornatskyi@gmail.com>
2021-12-11 20:18:13 -0800

    creating list node

--- src/main.c
+++ src/main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+struct ListNode {
+    int val;
+    struct ListNode* next;
+};
+// figure out why intellisence not working
 
 int main() {
 
-    printf("Hello World!\n");
+    struct ListNode head;
+    head.val = 1;
+    struct ListNode node2;
+    node2.val = 2;
+    node2.next = NULL;
+    head.next = &node2;
+
+
+    printf("%i", head.next->val);
 
 
     return 0;
+
+    
 }
+
